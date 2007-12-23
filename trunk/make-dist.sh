fc408c2fe467244ed584eb0dc642dca2add79d1c
kawa0117 <kawa0117@4b0939d0-ee41-0410-9078-99bb3c2717fb>
2007-12-23 07:27:38 +0000

    see http://www.kawa.net/works/perl/treepp/treepp-e.html

---
+++ trunk/make-dist.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+die () {
+    echo "$*" >&2
+    exit 1
+}
+doit () {
+    echo "\$ $*" >&2
+    $* || die "[ERROR:$?]"
+}
+
+doit wget -O t/example/index.rdf http://www.kawa.net/rss/index-e.rdf
+
+egrep -v '^t/.*\.t$' MANIFEST > MANIFEST~
+ls t/*.t >> MANIFEST~
+diff MANIFEST MANIFEST~ > /dev/null || doit /bin/mv -f MANIFEST~ MANIFEST
+/bin/rm -f MANIFEST~
+
+[ -f Makefile ] && doit make clean
+doit perl Makefile.PL
+doit make
+doit make disttest
+
+main=`grep 'lib/.*pm$' < MANIFEST | head -1`
+[ "$main" == "" ] && die "main module is not found in MANIFEST"
+doit pod2text $main > README
+
+doit make dist
+doit /bin/rm -fr blib pm_to_blib
+
+ls -lt *.tar.gz | head -1
